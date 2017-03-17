28f6603306faad20fad7b529ce43543c2c561390
kino273 <kino273@gmail.com>
2017-03-17 10:41:46 +0900

    copy src in Dockerfile

--- copy.sh
+++ copy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mkdir -p /usr/local/docker/src
+mkdir -p /usr/local/docker
 touch /usr/local/docker/ttt.devcopy.timestamp
 ls -lR /usr/local/docker/
 cp -R /usr/local/docker/* /usr/local/docker-host/
-cp -R /root/shairport-sync /usr/local/docker/src/
