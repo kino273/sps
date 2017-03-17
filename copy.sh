2fadb6e1dd8ea3b8e7ed124595308f6346d765ff
kino273 <kino273@gmail.com>
2017-03-17 10:23:05 +0900

    copy src as well

--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mkdir -p /usr/local/docker
+mkdir -p /usr/local/docker/src
 touch /usr/local/docker/ttt.devcopy.timestamp
 ls -lR /usr/local/docker/
 cp -R /usr/local/docker/* /usr/local/docker-host/
+cp -R /root/shairport-sync /usr/local/docker/src/
